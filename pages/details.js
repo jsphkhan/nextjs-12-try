5fe7b33e656cfd163e1c4a00b1d0fc11c769c9fa
Joseph <jsphkhan@gmail.com>
2022-06-12 19:16:34 +0400

    feat: added ssr data fetching

--- pages/details.js
+++ pages/details.js
@@ -1,8 +1,49 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 
+//import heavy vendor libs
+import moment from 'moment';
+
+import styles from '../styles/Details.module.css';
+
 export default function DetailsPage() {
-    return <>
-        <h1>Details Page</h1>
-        <Link href="/"><a>Back</a></Link>
-    </>
+  useEffect(() => {
+    const locale = moment.locale();
+    console.log(locale);
+    console.log(moment().format('DD MM YYYY'));
+  }, []);
+
+  return (
+    <div className={styles.detailscontainer}>
+      <h1>Details Page (SSG)</h1>
+      <Link href='/'>
+        <a>Back</a>
+      </Link>
+      <div>
+        <p>
+          We recommend using Static Generation (with and without data) whenever
+          possible because your page can be built once and served by CDN, which
+          makes it much faster than having a server render the page on every
+          request. You can use Static Generation for many types of pages,
+          including: Marketing pages Blog posts E-commerce product listings Help
+          and documentation You should ask yourself: "Can I pre-render this page
+          ahead of a user's request?" If the answer is yes, then you should
+          choose Static Generation.
+        </p>
+        <p>
+          Websites often use third-party scripts to include different types of
+          functionality into their site, such as analytics, ads, customer
+          support widgets, and consent management. However, this can introduce
+          problems that impact both user and developer experience: Some
+          third-party scripts are heavy on loading performance and can drag down
+          the user experience, especially if they are render-blocking and delay
+          any page content from loading Developers often struggle to decide
+          where to place third-party scripts in an application to ensure optimal
+          loading The Script component makes it easier for developers to place a
+          third-party script anywhere in their application while taking care of
+          optimizing its loading strategy.
+        </p>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 import Head from 'next/head';
 import Image from 'next/image';
@@ -6,52 +6,85 @@ import Link from 'next/link';
 
 //import heavy vendor libs
 import _ from 'lodash';
-import moment from 'moment';
 
 import styles from '../styles/Home.module.css';
 
-export default function HomePage() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    // make call
-    async function fetchData() {
-      try {
-        const response = await fetch('https://reqres.in/api/users?per_page=12');
-        const data = await response.json();
-        const users = _.get(data, 'data', {});
-        console.log(users);
-        setUsers(users);
-      } catch(err) {
-        console.log('Error when making call: ', err);
-      }
-    }
-    fetchData();
-
-    const locale = moment.locale();
-    console.log(locale);
-    console.log(moment().format('DD MM YYYY'));
-  }, []);
+export default function HomePage({ users }) {
+  // const [users, setUsers] = useState([]);
+  // useEffect(() => {
+  //   // make call
+  //   async function fetchData() {
+  //     try {
+  //       const response = await fetch('https://reqres.in/api/users?per_page=12');
+  //       const data = await response.json();
+  //       const users = _.get(data, 'data', {});
+  //       console.log(users);
+  //       setUsers(users);
+  //     } catch (err) {
+  //       console.log('Error when making call: ', err);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
     <>
       <Head>
         <title>Home Page</title>
       </Head>
-      <h1>Home Page</h1>
-      <Link href="/details"><a>Details</a></Link>
-      <ul className={styles.listcontainer}>
-        {users.map((user) => {
-          const id = _.get(user, 'id');
-          const name = `${_.get(user, 'first_name', '')} ${_.get(user, 'last_name', '')}`;
-          const avatar = _.get(user, 'avatar', '');
-          return (
-            <li key={id}>
-              <Image src={avatar} width={80} height={80} />
-              <span className={styles.name}>{name}</span>
-            </li>
-          )
-        })}
-      </ul>
+      <div className={styles.homecontainer}>
+        <h1>Home Page (SSR)</h1>
+        <Link href='/details'>
+          <a>Details</a>
+        </Link>
+        <ul className={styles.listcontainer}>
+          {users.map((user) => {
+            const id = _.get(user, 'id');
+            const name = `${_.get(user, 'first_name', '')} ${_.get(
+              user,
+              'last_name',
+              ''
+            )}`;
+            const avatar = _.get(user, 'avatar', '');
+            return (
+              <li key={id}>
+                <div className={styles.img}>
+                  <Image src={avatar} width={80} height={80} />
+                </div>
+                <div className={styles.name}>
+                  <span>{name}</span>
+                  <p>
+                    Websites often use third-party scripts to include different
+                    types of functionality into their site, such as analytics,
+                    ads, customer support widgets, and consent management.
+                  </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const response = await fetch('https://reqres.in/api/users?per_page=12');
+    const data = await response.json();
+    const users = _.get(data, 'data', {});
+    console.log(users);
+    return {
+      props: {
+        users,
+      },
+    };
+  } catch (err) {
+    console.log('Error when making call: ', err);
+    return {
+      props: {
+        users: [],
+      },
+    };
+  }
 }

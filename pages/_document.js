34b5d8f980040a2a01a7c8c3f528b33245858647
Joseph <jsphkhan@gmail.com>
2022-06-14 11:31:19 +0400

    feat: using roboto font

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
